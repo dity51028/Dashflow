4a5540a2e3f3407a9d26d83c3633072cdee9f341
Dipannita_Mondal <dipannitamisti10@gmail.com>
2024-09-02 17:30:31 +0530

    charts added finally

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,8 +1,20 @@
 import React from 'react'
+import ProjectStatistics from '../components/ProjectStatistics'
+import PlatformCard from '../components/PlatformCard'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div className='p-5'>
+      <div className='grid md:grid-cols-2 xl:grid-cols-4 gap-4'>
+      <ProjectStatistics/> 
+      <PlatformCard/>
+      <ProjectStatistics/> 
+      <PlatformCard/>
+      
+      </div>
+      
+    </div>
+   
   )
 }
 
